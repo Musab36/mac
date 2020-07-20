f1a3dca134c6417a60a41f29d9edb6bde665e33e
Musab36 <musabhd@gmail.com>
2020-07-20 16:29:45 +0200

    Lessons activity ui design

---
+++ app/src/main/java/com/salajim/musab/musabacademy/LessonsActivity.java
@@ -0,0 +1,23 @@
+package com.salajim.musab.musabacademy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class LessonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lessons);
+
+        // Setting up the custom toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+}
--- app/src/main/java/com/salajim/musab/musabacademy/MainActivity.java
+++ app/src/main/java/com/salajim/musab/musabacademy/MainActivity.java
@@ -1,14 +1,32 @@
 package com.salajim.musab.musabacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    CardView lessonsCard;
+    CardView signalsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lessonsCard = (CardView) findViewById(R.id.card1);
+        signalsCard = (CardView) findViewById(R.id.card2);
+
+        lessonsCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == lessonsCard) {
+            Intent intent = new Intent(this, LessonsActivity.class);
+            startActivity(intent);
+        }
     }
 }
